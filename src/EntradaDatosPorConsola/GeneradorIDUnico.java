d850459de5eced4a6cff0a6ff6cef653284bed15
jacke <jackeline.raam@gmail.com>
2026-09-17 13:06:12 -0600

    Correcciones del reto

--- src/EntradaDatosPorConsola/GeneradorIDUnico.java
+++ src/EntradaDatosPorConsola/GeneradorIDUnico.java
@@ -21,9 +21,9 @@ public class GeneradorIDUnico {
         int ramdom = random.nextInt(10000) + 1; // Genera un número aleatorio entre 1 y 9999
         String randomFormateado = String.format("%04d", ramdom); // Formatea el número con ceros a la izquierda
         
-        String nombreFormateado = nombre.substring(0, 2).toUpperCase(); // Primeras dos letras del nombre en mayúsculas
-        String apellidoFormateado = apellido.substring(0, 2).toUpperCase(); // Primeras dos letras del apellido en mayúsculas
-        String nacimientoFormateado = nacimiento.substring(2, 4); // Últimos dos dígitos del año de nacimiento         
+        String nombreFormateado = nombre.substring(0, 2).toUpperCase().trim(); // Primeras dos letras del nombre en mayúsculas
+        String apellidoFormateado = apellido.substring(0, 2).toUpperCase().trim(); // Primeras dos letras del apellido en mayúsculas
+        String nacimientoFormateado = nacimiento.substring(2, 4).trim(); // Últimos dos dígitos del año de nacimiento         
         
         System.out.printf("Hola " + nombre + "," + "\n\tTu ID único generado es:\n\t%s%s%s%s", nombreFormateado, apellidoFormateado, nacimientoFormateado, randomFormateado + "\n\tFelicidades!\n");
 
